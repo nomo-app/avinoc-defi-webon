fc7ec71cc3ea28c5e15b3ef9b272dcae36f09839
nomo-fe <dev2@nomo.app>
2024-04-17 12:40:22 +0200

    remove removeDuplicateWebOns because it is broken in some edge-cases when clicking on individual NFTs

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,25 @@ import MintingPage from "./app/minting/logic/MintingPage";
 import "./app/globals.css";
 import ClaimRewardsPage from "./app/claiming/logic/ClaimRewardsPage";
 import { HashRouter } from "react-router-dom";
-import { NomoManifest, nomo } from "nomo-webon-kit";
+import { nomo } from "nomo-webon-kit";
 
-async function removeDuplicateWebOns() {
-  const { manifests } = await nomo.getInstalledWebOns();
-  const ownManifest: NomoManifest = await nomo.getManifest();
-  const duplicateManifests = manifests.filter(
-    (manifest) =>
-      manifest.webon_id === "com.avinoc.defi" &&
-      manifest.webon_url !== ownManifest.webon_url
-  );
-  for (const manifest of duplicateManifests) {
-    await nomo.uninstallWebOn(manifest);
-    console.log(`Removed duplicate WebOn: ${manifest.webon_url}`);
-  }
-}
+// async function removeDuplicateWebOns() {
+//   const { manifests } = await nomo.getInstalledWebOns();
+//   const ownManifest: NomoManifest = await nomo.getManifest();
+//   const duplicateManifests = manifests.filter(
+//     (manifest) =>
+//       manifest.webon_id === "com.avinoc.defi" &&
+//       manifest.webon_url !== ownManifest.webon_url
+//   );
+//   for (const manifest of duplicateManifests) {
+//     await nomo.uninstallWebOn(manifest);
+//     console.log(`Removed duplicate WebOn: ${manifest.webon_url}`);
+//   }
+// }
 
 const App: React.FC = () => {
   useEffect(() => {
-    removeDuplicateWebOns();
+    // removeDuplicateWebOns();
     nomo.fallbackQRCode();
     nomo.registerOnWebOnVisible((_args: { cardMode: boolean }) => {
       nomo.checkForWebOnUpdate();
