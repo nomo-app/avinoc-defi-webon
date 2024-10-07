e32b2daff2b04f1707c8b6a65223c004cfdb25f8
nomo-fe <dev2@nomo.app>
2024-10-07 16:31:33 +0200

    chore: upgrade to webon-cli 0.2.0

--- nomo_cli.config.cjs
+++ nomo_cli.config.cjs
@@ -1,8 +1,13 @@
+const sshHost = process.env.SSH_TARGET;
+if (!sshHost) {
+  throw new Error("SSH_TARGET not set");
+}
+
 const nomoCliConfig = {
   deployTargets: {
     production: {
       rawSSH: {
-        sshHost: process.env.SSH_TARGET,
+        sshHost,
         sshBaseDir: "/var/www/production_webons/avinocdefi/",
         publicBaseUrl: "https://defi.avinoc.com",
         hybrid: true,
